84ca35d12562c4d69903578a5765d71a03ba26ab
bensweett <bensweett@192.168.1.237>
2014-10-02 17:17:54 -0400

    Added exit command and handled some exceptions. Started adding comments.

--- ctransfer/src/com/ctransfer/application/Application.java
+++ ctransfer/src/com/ctransfer/application/Application.java
@@ -2,21 +2,41 @@ package com.ctransfer.application;
 
 import java.util.Scanner;
 
-import com.ctransfer.client.Client;
 import com.ctransfer.client.SocketClient;
-import com.ctransfer.server.Server;
 import com.ctransfer.server.SocketServer;
 
+/**
+ * The ctransfer program implements an application that allows users
+ * to transfer files between a server and a client.
+ *
+ * @author Ben Sweett & Colin Tinney
+ * @version 1.0
+ * @since 2014-09-29
+ */
 public class Application {
 	
+	/**
+	 * Print function for simply printing a string to the standard 
+	 * output
+	 * 
+	 * @param String to be printed
+	 * @return void
+	 */
 	public static void print(String s) {
 		System.out.println(s);
 	}
 	
+	/**
+	 * Init function allows for user to select which program to
+	 * run. Waits for user to input a 1 for server and a 2 for
+	 * client. Opens a Server and client socket on port 9000.
+	 * 
+	 * @param None
+	 * @return void
+	 */
 	public static void init() {
 		
-		print("Network file service launched.");
-		print("Initialized at: " + System.getProperty("user.dir"));
+		print("\nNetwork file service launched.");
 		print("--------------------------------");
 		print("Please make a selection.");
 		print("\t1. Server");
@@ -28,11 +48,11 @@ public class Application {
 		
 			Integer selection = sc.nextInt();
 			if (selection == 1) {
-				Server server = new SocketServer(9000);	
+				SocketServer server = new SocketServer(9000);	
 				server.start();
 				
 			} else if (selection == 2) {
-				Client client = new SocketClient("localhost", 9000);
+				SocketClient client = new SocketClient("localhost", 9000);
 				client.start();
 				
 			}
@@ -44,6 +64,12 @@ public class Application {
 		
 	}
 
+	/**
+	 * Main execution point for the application
+	 * 
+	 * @param String[] arguments
+	 * @return void
+	 */
 	public static void main(String[] args) {
 		
 		init();
